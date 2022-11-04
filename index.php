ac4fd156638fe42c244bbdb19166f54d16e07da7
Paulo Victor <101723532+PaulinIFSP@users.noreply.github.com>
2022-11-04 10:36:54 -0300

    Update index.php

--- index.php
+++ index.php
@@ -17,41 +17,51 @@
 </head>
 
 <body>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-
-   <a href="index.php"><img src="img/logo.png" width="150px" height="100px"></a>
-    <input class="form-control mr-sm-2" type="pesquisar" placeholder="Pesquisar" aria-label="Search" id="campoPesquisa">
-    <button class="btn btn-outline-primary my-2 my-sm-0" type="submit" id="pesquisar">Pesquisar</button>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="botoes">
-      <a href="login_formulario.php"><button class="btn btn-primary my-2 my-sm-0" type="submit" id="btnEntrar">
-        Entrar</button></a>
-      <a href="usuario_formulario.php"><button class="btn btn-primary my-2 my-sm-0" type="submit"
-          id="btnCadastrar">Cadastrar</button></a>
-    </div>
-    <div class="card-body text-right" id="icon1">
-      <a href="perfil_usuario.php"><i class="bi bi-person"></i></a>
-    </div>
-  </nav>
+    <!-- TOPO-->
+    <?php
+        include 'includes/topo.php';
+    ?>
   <div class="container-fluid">
     <div class="row tamanho">
       <div class="col-2 borda" id="menu">
       <li>
-        <a href=""><i class="bi bi-house-fill">Inicio</i></a>
+        <a href="index.php"><i class="bi bi-house-fill">Inicio</i></a>
       </li>
       <li>
         <a href=""><i class="bi bi-person-plus">Seguindo</i></a>
       </li>
       <li>
-        <a href=""><i class="bi bi-book">Matérias</i></a>
+        <a href="materias.php"><i class="bi bi-book">Matérias</i></a>
       </li>
       <li>
         <p style="font-size: 15pt;">Faça login para poder curtir, comentar e compartilhar.</p>
       </li>
-    </div>
+      </div>
+
+
+      <!---Video-->
+      <div class="col-9" >
+        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <blockquote class="tiktok-embed" cite="https://www.tiktok.com/@giscomgizmatematica/video/7114067961312234758" data-video-id="7114067961312234758" style="max-width: 605px;min-width: 325px;" > <section> <a target="_blank" title="@giscomgizmatematica" href="https://www.tiktok.com/@giscomgizmatematica?refer=embed">@giscomgizmatematica</a> Acertou? <a title="matematica" target="_blank" href="https://www.tiktok.com/tag/matematica?refer=embed">#matematica</a> <a title="aprendanotiktok" target="_blank" href="https://www.tiktok.com/tag/aprendanotiktok?refer=embed">#aprendanotiktok</a> <a title="giscomgiz" target="_blank" href="https://www.tiktok.com/tag/giscomgiz?refer=embed">#giscomgiz</a> <a target="_blank" title="♬ som original - Gis com giz" href="https://www.tiktok.com/music/som-original-7114067962923485958?refer=embed">♬ som original - Gis com giz</a> </section> </blockquote> <script async src="https://www.tiktok.com/embed.js"></script>
+            </div>
+            <div class="carousel-item">
+              <blockquote class="tiktok-embed" cite="https://www.tiktok.com/@profmylenemendes/video/7094587516514225413" data-video-id="7094587516514225413" style="max-width: 605px;min-width: 325px;" > <section> <a target="_blank" title="@profmylenemendes" href="https://www.tiktok.com/@profmylenemendes?refer=embed">@profmylenemendes</a> <a title="adicaodefracoes" target="_blank" href="https://www.tiktok.com/tag/adicaodefracoes?refer=embed">#adicaodefracoes</a> <a title="fyyyyyyyyyyyyyyyy" target="_blank" href="https://www.tiktok.com/tag/fyyyyyyyyyyyyyyyy?refer=embed">#fyyyyyyyyyyyyyyyy</a> <a title="fypage" target="_blank" href="https://www.tiktok.com/tag/fypage?refer=embed">#fypage</a>  <a title="vaiprafy" target="_blank" href="https://www.tiktok.com/tag/vaiprafy?refer=embed">#vaiprafy</a> <a title="matematica" target="_blank" href="https://www.tiktok.com/tag/matematica?refer=embed">#matematica</a> <a target="_blank" title="♬ Believe Me - Navos" href="https://www.tiktok.com/music/Believe-Me-6925124562560485378?refer=embed">♬ Believe Me - Navos</a> </section> </blockquote> <script async src="https://www.tiktok.com/embed.js"></script>
+            </div>
+            <div class="carousel-item">
+              <blockquote class="tiktok-embed" cite="https://www.tiktok.com/@giscomgizmatematica/video/7020458019104361734" data-video-id="7020458019104361734" style="max-width: 605px;min-width: 325px;" > <section> <a target="_blank" title="@giscomgizmatematica" href="https://www.tiktok.com/@giscomgizmatematica?refer=embed">@giscomgizmatematica</a> ❌Não erre mais! ❌ <a title="giscomgiz" target="_blank" href="https://www.tiktok.com/tag/giscomgiz?refer=embed">#giscomgiz</a> <a title="math" target="_blank" href="https://www.tiktok.com/tag/math?refer=embed">#math</a> <a title="matematica" target="_blank" href="https://www.tiktok.com/tag/matematica?refer=embed">#matematica</a> <a title="divisao" target="_blank" href="https://www.tiktok.com/tag/divisao?refer=embed">#divisao</a> <a target="_blank" title="♬ som original - Gis com giz" href="https://www.tiktok.com/music/som-original-7020457908898925317?refer=embed">♬ som original - Gis com giz</a> </section> </blockquote> <script async src="https://www.tiktok.com/embed.js"></script>
+            </div>
+          </div>
+        <button class="carousel-control-prev" type="button" data-target="#carouselExampleControls" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-target="#carouselExampleControls" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </button>
+        </div>
+      </div>
 </body>
 </html>
